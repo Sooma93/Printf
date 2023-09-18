774ee6892ee477f5f5e05f60b4c3ce25a1f02f29
sooma93 <tasneemabasit5@gmail.com>
2023-09-17 23:33:09 -0700

    pr

--- _printf.c
+++ _printf.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
-/*
+/**
  * _printf - replica of printf function.
  * @format: Character string to be printed.
  *
@@ -10,52 +10,36 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
+
+	unsigned int i, s_count,  count = 0;
+	 va_list args;
+
 	va_start(args, format);
 
-	int printed_chars;
-
-	printed_chars = 0;
-
-	while (*format != '\0')
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (*format == '%')
-		{
-			format++;
-
-			switch (*format)
-			{
-				case 'c':
-				{
-					int c = va_arg(args, int);
-					putchar(c);
-					printed_chars++;
-					break;
-				}
-				case 's':
-				{
-					char *s = va_arg(args, char*);
-					fputs(s, stdout);
-					printed_chars += strlen(s);
-					break;
-                }
-				case '%':
-				{
-					putchar('%');
-					printed_chars++;
-					break;
-				}
-			}
-		}
-		else
-		{
-			putchar(*format);
-			printed_chars++;
-		}
-		format++;
+	if (format[i] != '%')
+	{
+	print_char(format[i]);
 	}
-
+	else if (format[i] == '%' && format[i + 1] == 'c')
+	{
+	print_char(va_arg(args, int));
+	i++;
+	}
+	else if (format[i + 1] == 's')
+	{
+	s_count = puts(va_arg(args, char *));
+	i++;
+	count += (s_count - 1);
+	}
+	else if (format[i + 1] == '%')
+	{
+	print_char('%');
+	}
+	count += 1;
+	}
 	va_end(args);
-
-	return (printed_chars);
+	return (count);
 }
+
