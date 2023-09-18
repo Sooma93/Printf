b3ae31aab1bdd29738eef77f0bab8c6b4a2dfe4a
sooma93 <tasneemabasit5@gmail.com>
2023-09-18 00:27:19 -0700

    r

--- _printf.c
+++ _printf.c
@@ -12,33 +12,34 @@ int _printf(const char *format, ...)
 {
 
 	unsigned int i, s_count,  count = 0;
-	 va_list args;
+	va_list args;
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 	va_start(args, format);
-	return (-1);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-	if (format[i] != '%')
-	{
-	print_char(format[i]);
-	}
-	else if (format[i + 1] == 'c')
-	{
-	print_char(va_arg(args, int));
-	i++;
-	}
-	else if (format[i + 1] == 's')
-	{
-	s_count = putss(va_arg(args, char *));
-	i++;
-	count += (s_count - 1);
-	}
-	else if (format[i + 1] == '%')
-	{
-	print_char('%');
-	}
-	count += 1;
+		if (format[i] != '%')
+		{
+			print_char(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			print_char(va_arg(args, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			s_count = putss(va_arg(args, char *));
+			i++;
+			count += (s_count - 1);
+		}
+		else if (format[i + 1] == '%')
+		{
+			print_char('%');
+		}
+		count += 1;
 	}
 	va_end(args);
 	return (count);
