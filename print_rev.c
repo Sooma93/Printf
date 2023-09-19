3c2da5926eb945cf821e34f44acf5d9e3e02d6f6
0xayman <aymantarig17@gmail.com>
2023-09-19 19:10:28 +0200

    handle reverse order

---
+++ print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_rev - prints a string in reverse order.
+* @l: list of arguments.
+* @f: pointer to struct flags.
+* Return: number of characters printed.
+*/
+int print_rev(va_list l, flags_t *f)
+{
+	int i = 0, j;
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	if (!s)
+		s = "(null)";
+
+	while (s[i])
+		i++;
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
+
+	return (i);
+}
